0dc77f2016d4b1f74857ea5b60a7822ca0035c08
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-04-25 11:55:14 +0100

    updated albums model with functions

---
+++ client/src/models/albums.js
@@ -0,0 +1,51 @@
+const PubSub = require('../helpers/pub_sub.js');
+const RequestHelper = require('../helpers/request_helper.js');
+
+class Albums {
+
+  constructor() {
+
+    this.data = null;
+
+  }
+
+  getData() {
+    const url = 'http://localhost:3000/albums';
+    const request = new RequestHelper(url);
+    request.get()
+      .then((data) => {
+        this.data = data;
+        PubSub.publish('Albums:DB-data-loaded', this.data);
+      });
+  }
+
+  postData(entry) {
+    const url = 'http://localhost:3000/albums';
+    const request = new RequestHelper(url);
+    request.post(entry)
+      .then((entries) => {
+        PubSub.publish('Albums:DB-data-loaded', entries);
+      });
+  }
+
+  putData(entry) {
+    const url = 'http://localhost:3000/albums';
+    const request = new RequestHelper(url);
+    request.put(entry)
+      .then((entries) => {
+        PubSub.publish('Albums:DB-data-loaded', entries);
+      });
+  }
+
+  deleteData(entry) {
+    const url = 'http://localhost:3000/albums';
+    const request = new RequestHelper(url);
+    request.delete(entry)
+      .then((entries) => {
+        PubSub.publish('Albums:DB-data-loaded', entries);
+      });
+  }
+
+}
+
+module.exports = SwimTimes;
