5a859b7f45ef1a6336e203e51ef1bca61563887f
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-04-25 11:50:01 +0100

    updated request helper with more functions

--- client/src/helpers/request_helper.js
+++ client/src/helpers/request_helper.js
@@ -18,6 +18,25 @@ class RequestHelper{
       .then((response) => response.json());
   }
 
+
+  put(payload) {
+    return fetch(this.url, {
+      method: 'PUT',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((response) => response.json());
+  }
+
+  delete(payload) {
+    return fetch(this.url, {
+      method: 'DELETE',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((response) => response.json());
+  }
+
 }
 
 module.exports = RequestHelper;
