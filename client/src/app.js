72ccc8389634547ca42cc2b80a391c21f6773c13
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-04-25 13:16:09 +0100

    fixed json error, updated app js

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,9 @@
+const Album = require('./models/albums.js');
+const AlbumsView = require('./views/albums_view.js')
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("JavaScript Loaded");
-  const Album = require('./models/albums.js');
 
   const album = new Album();
-
+  const albumsView = new AlbumsView();
 });
